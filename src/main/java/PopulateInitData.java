7526a5c1310ee3fa342449d91c133372481826c3
Maikel Chandika <mkdika@gmail.com>
2017-07-06 09:28:52 +0700

    first init.

---
+++ src/main/java/PopulateInitData.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author Maikel Chandika <mkdika@gmail.com>
+ */
+public class PopulateInitData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
